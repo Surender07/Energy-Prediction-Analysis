bf36f5f7d612be44b6dae0f06ff32aa28176a5de
AYUSH PATEL <ayushpatel221002@gmail.com>
2024-06-18 03:11:35 +1000

    Merged EDA graphs

--- EDA.R
+++ EDA.R
@@ -1,6 +1,5 @@
 data <- read.csv("UCI-electricity/UCI_data.csv")
 
-
 # Display the first few rows of the data
 head(data)
 
@@ -28,35 +27,50 @@ colSums(is.na(data))
 colMeans(is.na(data)) * 100
 
 
-# Histograms for temperature variables
-hist(data$T1, main="Histogram of Temperature in Kitchen", xlab="T1 (Celsius)", col="blue")
-hist(data$T2, main="Histogram of Temperature in Living Room", xlab="T2 (Celsius)", col="green")
-hist(data$T3, main="Histogram of Temperature in Laundry Room", xlab="T3 (Celsius)", col="red")
-hist(data$T4, main="Histogram of Temperature in Office Room", xlab="T4 (Celsius)", col="purple")
-hist(data$T5, main="Histogram of Temperature in Bathroom", xlab="T5 (Celsius)", col="orange")
-hist(data$T6, main="Histogram of Temperature Outside North", xlab="T6 (Celsius)", col="yellow")
-hist(data$T7, main="Histogram of Temperature in Ironing Room", xlab="T7 (Celsius)", col="pink")
-hist(data$T8, main="Histogram of Temperature in Teenager Room 2", xlab="T8 (Celsius)", col="cyan")
-hist(data$T9, main="Histogram of Temperature in Parents Room", xlab="T9 (Celsius)", col="brown")
-hist(data$T_out, main="Histogram of Temperature Outside (Chievres)", xlab="T_out (Celsius)", col="grey")
+# Set up the plotting area for multiple plots
+par(mfrow=c(3, 1), mar=c(4, 4, 2, 1), oma=c(0, 0, 2, 0))
 
-# Histograms for humidity variables
-hist(data$RH_1, main="Histogram of Humidity in Kitchen", xlab="RH_1 (%)", col="blue")
-hist(data$RH_2, main="Histogram of Humidity in Living Room", xlab="RH_2 (%)", col="green")
-hist(data$RH_3, main="Histogram of Humidity in Laundry Room", xlab="RH_3 (%)", col="red")
-hist(data$RH_4, main="Histogram of Humidity in Office Room", xlab="RH_4 (%)", col="purple")
-hist(data$RH_5, main="Histogram of Humidity in Bathroom", xlab="RH_5 (%)", col="orange")
-hist(data$RH_6, main="Histogram of Humidity Outside North", xlab="RH_6 (%)", col="yellow")
-hist(data$RH_7, main="Histogram of Humidity in Ironing Room", xlab="RH_7 (%)", col="pink")
-hist(data$RH_8, main="Histogram of Humidity in Teenager Room 2", xlab="RH_8 (%)", col="cyan")
-hist(data$RH_9, main="Histogram of Humidity in Parents Room", xlab="RH_9 (%)", col="brown")
-hist(data$RH_out, main="Histogram of Humidity Outside (Chievres)", xlab="RH_out (%)", col="grey")
+# Plot the histograms for temperature variables
+par(mfrow=c(2, 5))
+hist(data$T1, main="Kitchen", xlab="T1 (Celsius)", col="blue")
+hist(data$T2, main="Living Room", xlab="T2 (Celsius)", col="green")
+hist(data$T3, main="Laundry Room", xlab="T3 (Celsius)", col="red")
+hist(data$T4, main="Office Room", xlab="T4 (Celsius)", col="purple")
+hist(data$T5, main="Bathroom", xlab="T5 (Celsius)", col="orange")
+hist(data$T6, main="Outside North", xlab="T6 (Celsius)", col="yellow")
+hist(data$T7, main="Ironing Room", xlab="T7 (Celsius)", col="pink")
+hist(data$T8, main="Teenager Room 2", xlab="T8 (Celsius)", col="cyan")
+hist(data$T9, main="Parents Room", xlab="T9 (Celsius)", col="brown")
+hist(data$T_out, main="Outside Chievres", xlab="T_out (Celsius)", col="grey")
 
-# Additional numerical variables histograms
-hist(data$Press_mm_hg, main="Histogram of Pressure (Chievres)", xlab="Press_mm_hg (mm Hg)", col="blue")
-hist(data$Windspeed, main="Histogram of Wind Speed (Chievres)", xlab="Windspeed (m/s)", col="green")
-hist(data$Visibility, main="Histogram of Visibility (Chievres)", xlab="Visibility (km)", col="red")
-hist(data$Tdewpoint, main="Histogram of Dewpoint Temperature (Chievres)", xlab="Tdewpoint (Celsius)", col="purple")
-hist(data$rv1, main="Histogram of Random Variable 1", xlab="rv1", col="orange")
-hist(data$rv2, main="Histogram of Random Variable 2", xlab="rv2", col="yellow")
-hist(data$TARGET_energy, main="Histogram of Energy Use (Wh)", xlab="TARGET_energy (Wh)", col="pink")
+# Add an overall title for the temperature histograms
+mtext("Histograms of Temperature Variables", outer = TRUE, line = -2, cex = 1.5)
+
+# Plot the histograms for humidity variables
+par(mfrow=c(2, 5))
+hist(data$RH_1, main="Kitchen", xlab="RH_1 (%)", col="blue")
+hist(data$RH_2, main="Living Room", xlab="RH_2 (%)", col="green")
+hist(data$RH_3, main="Laundry Room", xlab="RH_3 (%)", col="red")
+hist(data$RH_4, main="Office Room", xlab="RH_4 (%)", col="purple")
+hist(data$RH_5, main="Bathroom", xlab="RH_5 (%)", col="orange")
+hist(data$RH_6, main="Outside North", xlab="RH_6 (%)", col="yellow")
+hist(data$RH_7, main="Ironing Room", xlab="RH_7 (%)", col="pink")
+hist(data$RH_8, main="Teenager Room 2", xlab="RH_8 (%)", col="cyan")
+hist(data$RH_9, main="Parents Room", xlab="RH_9 (%)", col="brown")
+hist(data$RH_out, main="Outside Chievres", xlab="RH_out (%)", col="grey")
+
+# Add an overall title for the humidity histograms
+mtext("Histograms of Humidity Variables", outer = TRUE, line = -22, cex = 1.5)
+
+# Plot the histograms for additional numerical variables
+par(mfrow=c(2, 3))
+hist(data$Press_mm_hg, main="Pressure (Chievres)", xlab="Press_mm_hg (mm Hg)", col="blue")
+hist(data$Windspeed, main="Wind Speed (Chievres)", xlab="Windspeed (m/s)", col="green")
+hist(data$Visibility, main="Visibility (Chievres)", xlab="Visibility (km)", col="red")
+hist(data$Tdewpoint, main="Dewpoint (Chievres)", xlab="Tdewpoint (Celsius)", col="purple")
+hist(data$rv1, main="Random Variable 1", xlab="rv1", col="orange")
+hist(data$rv2, main="Random Variable 2", xlab="rv2", col="yellow")
+hist(data$TARGET_energy, main="Energy Use (Wh)", xlab="TARGET_energy (Wh)", col="pink")
+
+# Add an overall title for the additional numerical variable histograms
+mtext("Histograms of Additional Numerical Variables", outer = TRUE, line = -42, cex = 1.5)
